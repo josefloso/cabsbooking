eee9fd61dcdbbad9c3dcffccf354a2301ee1b888
Josef <sendypc@Josephs-MacBook-Air.local>
2020-02-04 08:39:54 +0300

    first cabs commit

---
+++ src/main/java/com/ecabs/booking/config/AppConfig.java
@@ -0,0 +1,7 @@
+package com.ecabs.booking.config;
+
+/*
+  author joseph kariuki created on 1/26/20
+*/
+public class AppConfig {
+}
---
+++ src/main/java/com/ecabs/booking/domain/repository/TripWaypointRepository.java
@@ -0,0 +1,7 @@
+package com.ecabs.booking.domain.repository;
+
+/*
+  author joseph kariuki created on 1/26/20
+*/
+public class TripWaypointRepository {
+}
